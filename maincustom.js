e394f0833af8900440a26aeb63017656c539b3de
brkfstkng <97113790+brkfstkng@users.noreply.github.com>
2024-06-09 13:26:57 -0400

    Update maincustom.js

--- maincustom.js
+++ maincustom.js
@@ -34,7 +34,7 @@ $(document).ready(function () {
     "animate": true,
     "patternWidth": 100,
     "patternHeight": 100,
-    "grainOpacity": 0.04,
+    "grainOpacity": 0.06,
     "grainDensity": 1,
     "grainWidth": 1,
     "grainHeight": 1
